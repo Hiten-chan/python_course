8063bff65a36edf6be236f5cc64df539cf37dbff
Alexandra <hiten-hemyra@mail.ru>
2015-09-18 00:38:47 +0400

    homework1_task1

--- homeworks/homework1/task1.py
+++ homeworks/homework1/task1.py
@@ -11,7 +11,7 @@ if word == "óòşã":
 elif word == "ëîæêà":
     if number % 10 == 1 and number != 11 and number % 100 != 11:
         print(number, word)
-    elif ( number % 10 == 2 or number % 10 == 3 or number % 10 == 4) and number % 100 != 12 and number % 100 != 13 and number % 100 != 14:
+    elif (number % 10 == 2 or number % 10 == 3 or number % 10 == 4) and number % 100 != 12 and number % 100 != 13 and number % 100 != 14:
         print(number, word[0:4] + "è")
     else:
         print(number, word[0:3] + "åê")
