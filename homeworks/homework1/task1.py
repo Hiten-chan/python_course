37ba14dd201a5592d51bdecdcd7fc66106de1836
Hiten-chan <hiten-hemyra@mail.ru>
2015-09-18 00:22:54 +0400

    homework1_task0

--- homeworks/homework1/task1.py
+++ homeworks/homework1/task1.py
@@ -1,9 +1,17 @@
-lst = input().split(" ")
-numbers = 0
-n = 0
+word = input()
+number = int(input())
 
-for i in lst:
-    numbers += int(i)
-    n += 1
-
-print(numbers/n)
+if word == "óòşã":
+    if number%10 == 1 and number != 11 and number%100 != 11:
+        print (number, word)
+    elif (number%10 == 2 or number%10 == 3 or number%10 == 4) and number%100 != 12 and number%100 != 13 and number%100 != 14:
+        print (number, word+"à")
+    else:
+        print (number, word+"îâ")
+elif word == "ëîæêà":
+    if number%10 == 1 and number != 11 and number%100 != 11:
+        print (number, word)
+    elif (number%10 == 2 or number%10 == 3 or number%10 == 4) and number%100 != 12 and number%100 != 13 and number%100 != 14:
+        print (number, word[0:4]+"è")
+    else:
+        print (number, word[0:3]+"åê")
