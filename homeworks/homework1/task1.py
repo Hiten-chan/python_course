47b748545549ec17bbe85734eb0228f41b6d285c
Alexandra <hiten-hemyra@mail.ru>
2015-09-26 15:08:54 +0300

    homework1_task1

--- homeworks/homework1/task1.py
+++ homeworks/homework1/task1.py
@@ -4,14 +4,14 @@ number = int(input())
 if word == "утюг":
     if number % 10 == 1 and number != 11 and number % 100 != 11:
         print(number, word)
-    elif (number % 10 == 2 or number % 10 == 3 or number % 10 == 4) and number % 100 != 12 and number % 100 != 13 and number % 100 != 14:
+    elif number % 10 == [2, 3, 4] and number % 100 != [12, 13, 14]:
         print(number, word + "2")
     else:
         print(number, word + "ов")
 elif word == "ложка":
     if number % 10 == 1 and number != 11 and number % 100 != 11:
         print(number, word)
-    elif (number % 10 == 2 or number % 10 == 3 or number % 10 == 4) and number % 100 != 12 and number % 100 != 13 and number % 100 != 14:
+    elif number % 10 == [2, 3, 4] and number % 100 != [12, 13, 14]:
         print(number, word[0:4] + "и")
     else:
         print(number, word[0:3] + "ек")
