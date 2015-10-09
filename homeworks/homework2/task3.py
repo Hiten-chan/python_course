87fbc01e2b3d37e043c209ed8389a0ab3c6cf6bc
Alexandra <hiten-hemyra@mail.ru>
2015-10-10 01:15:36 +0300

    homework2_task3

---
+++ homeworks/homework2/task3.py
@@ -0,0 +1,13 @@
+s = input().split(" ")
+a = int(s[0])
+b = int(s[1])
+
+def euclid(a, b):
+    if b == 0:
+        return a
+    elif a % b == 0:
+        return b
+    else:
+        return euclid(b, a % b)
+
+print(euclid(a, b))
