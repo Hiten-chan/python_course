14c5bbfd14e20d0da1795319393944f509602110
Alexandra <hiten-hemyra@mail.ru>
2015-10-10 01:04:10 +0300

    homework2_task1

---
+++ homeworks/homework2/task1.py
@@ -0,0 +1,19 @@
+s = input().split(" ")
+n = int(s[0])
+k = int(s[1])
+
+def fac(n):
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+def combinations(n, k):
+    if k > n:
+        return 0
+    elif k == n or k == 0:
+        return 1
+    else:
+        return (fac(n) / (fac(k) * fac(n - k)))
+
+print(int(combinations(n, k)))
