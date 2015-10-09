e8bcab8ed2b261e5b537586b88c94f67723330c9
Alexandra <hiten-hemyra@mail.ru>
2015-10-09 23:33:17 +0300

    homework1_task1

--- homeworks/homework1/task1.py
+++ homeworks/homework1/task1.py
@@ -11,7 +11,7 @@ if word == "утюг":
 elif word == "ложка":
     if number % 10 == 1 and number != 11 and number % 100 != 11:
         print(number, word)
-    elif number % 10 == [2, 3, 4] and number % 100 != [12, 13, 14]:
+    elif number % 10 in [2, 3, 4] and number % 100 not in [12, 13, 14]:
         print(number, word[0:4] + "и")
     else:
         print(number, word[0:3] + "ек")
