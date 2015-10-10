6a5fc12b255e72dfe4c9001887508a90ebf47b90
Alexandra <hiten-hemyra@mail.ru>
2015-10-10 16:09:51 +0300

    homework2_task0

--- homeworks/homework2/task0.py
+++ homeworks/homework2/task0.py
@@ -2,22 +2,25 @@
 word = str(input())
 n = int(input())
 
-if word == "ложка":
-    w = ["ложка", "ложки", "ложек"]
-elif word == "утюг":
-    w = ["утюг", "утюга", "утюгов"]
-elif word == "гармошка":
-    w = ["гармошка", "гармошки", "гармошек"]
-elif word == "чайник":
-    w = ["чайник", "чайника", "чайников"]
-
-
 def plural(number, words):
     if number % 10 == 1 and number != 11 and number % 100 != 11:
-        print(number, words[0])
+        return(words[0])
     elif number % 10 in [2, 3, 4] and number % 100 not in [12, 13, 14]:
-        print(number, words[1])
+        return(words[1])
     else:
-        print(number, words[2])
+        return(words[2])
 
-plural(n, w)
+if word == "ложка":
+    w = ["ложка", "ложки", "ложек"]
+    cor_word = plural(n, w)
+elif word == "утюг":
+    w = ["утюг", "утюга", "утюгов"]
+    cor_word = plural(n, w)
+elif word == "гармошка":
+    w = ["гармошка", "гармошки", "гармошек"]
+    cor_word = plural(n, w)
+elif word == "чайник":
+    w = ["чайник", "чайника", "чайников"]
+    cor_word = plural(n, w)
+
+print(n, cor_word)
