549e2b51b6f046c03a1cf0c821780f402433862b
Alexandra <hiten-hemyra@mail.ru>
2015-11-07 17:50:14 +0300

    Update task1.py

--- exam1/task1.py
+++ exam1/task1.py
@@ -1,1 +1,18 @@
+f1 = open("yazkora.txt", 'r')
+l = f1.read().replace("\n", "").split(".")
+f1.close()
 
+out = []
+
+for i in l:
+    p = i.split(" ")
+    out.append([word for word in p if word.endswith('yo')])
+
+f2 = open('answer.txt', 'w' )
+
+for item in out:
+    for j in item:
+        f2.write(j + " ")
+    f2.write("\n")
+
+f2.close()
