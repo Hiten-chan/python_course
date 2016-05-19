649edaf2812ef05b5a83bc056715b02779ecb454
Alexandra <hiten-hemyra@mail.ru>
2016-05-19 15:50:05 +0300

    Update requests.sql

--- homeworks_spring/homework5/requests.sql
+++ homeworks_spring/homework5/requests.sql
@@ -1,40 +1,86 @@
-1) select * from users;
+1) Всю информацию о всех пользователях (все колонки в любом
+порядке)
 
-2) select users.name, count(*) from users;
+select * from users;
 
-3) select users.name, birth_date, count(*) from users 
+
+2) Количество всех пользователей (число)
+
+select users.name, count(*) from users;
+
+
+3) Количество пользователей 40 лет или старше (число). Чтобы
+сравнить столбец таблицы с конкретной датой можно использо-
+вать функцию date – where birth_date <= date(”2000-12-20”) с да-
+той в формате ”YYYY-MM-DD”.
+
+select users.name, birth_date, count(*) from users 
 where birth_date <= date('1976-05-20');
 
-4) select users.country, count(*) from users 
+
+4) Страна + количество пользователей из данной страны (стра-
+на|количество)
+
+select users.country, count(*) from users 
 group by country;
 
-5) select users.name, count(*) as c from users 
+
+5) Придумайте, как проверить, есть ли люди с одинаковым именем
+(в любом удобном формате)
+
+select users.name, count(*) as c from users 
 group by name 
 order by c desc limit 1;
 
-6) select orders.created datetime, count(*) from orders 
+
+6) Количество заказов в 2016 году (число)
+
+select orders.created datetime, count(*) from orders 
 where created >= '2016-01-01 00:00:00';
 
-7) select date(created) as f, count(*) as a from orders 
+
+7) День, когда совершили наибольшее число заказов (день|число
+заказов)
+
+select date(created) as f, count(*) as a from orders 
 group by f 
 order by a desc limit 1;
 
-8) select 100 * sum (case paid when 0 then 1 else 0 end) / count(paid) from orders;
 
-9) select * from goods where name like '%bread%';
+8) Процент неоплаченных заказов (число)
 
-10) select goods.id, goods.name, count(*) as a from goodsinorders 
+select 100 * sum (case paid when 0 then 1 else 0 end) / count(paid) from orders;
+
+
+9) Всю информацию о хлебе среди товаров. Используйте конструк-
+цию where name like ”%bread%”. Ссылка. (все колонки в любом
+порядке)
+
+select * from goods where name like '%bread%';
+
+
+10) Самые 10 популярных товаров (встречаемость в GoodsInOrders)
+(id|name|количество)
+
+select goods.id, goods.name, count(*) as a from goodsinorders 
 inner join goods on good_id = id 
 group by good_id 
 order by a desc 
 limit 10;
 
-11) select sum(price) from goods 
+
+11) Чистая выручка в 2016 году. Нужно учитывать только оплачен-
+ные заказы (число)
+
+select sum(price) from goods 
 inner join orders on orders.id = order_id 
 inner join goodsinorders on goods.id = good_id 
 where paid = 1;
 
-12) select Goods.id, Goods.name from Goods 
+
+12) Самые 10 популярных товаров среди женщин (id|название)
+
+select Goods.id, Goods.name from Goods 
 inner join Users on Users.id = user_id 
 inner join Orders on order_id = Orders.id 
 inner join GoodsInOrders on Goods.id = good_id 
@@ -43,7 +89,10 @@ group by Goods.name
 order by count(*) desc 
 limit 10;
 
-13) select Users.id, Users.name from Users 
+
+13) Пользователь, который купил больше всего килограмм (id|имя)
+
+select Users.id, Users.name from Users 
 inner join Goods on Goods.id = good_id 
 inner join GoodsInOrders on Orders.id = order_id
 inner join Orders on Users.id = user_id 
